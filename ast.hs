700b8a85edd303eeaf25c373dae29e0e195801e4
Nicolas Mohr <Nico.Mohr@gmx.net>
2019-12-05 00:47:01 +0100

    added cleaner approach using an ast

---
+++ ast.hs
@@ -0,0 +1,104 @@
+import Control.Applicative hiding (many)
+import Text.ParserCombinators.ReadP
+import qualified Data.List.Split
+
+data Token = Pipe | Colon | TypeName [Char] | FieldName [Char] | ParLeft | ParRight | BraceLeft | BraceRight 
+    deriving (Show, Eq)
+
+parseTypeNameString :: ReadP [Char]
+parseTypeNameString = do
+    capitalLetter <- satisfy (\char -> char >= 'A' && char <= 'Z')
+    remainingLetters <- many (satisfy (\char -> char >= 'A' && char <= 'z'))
+    return (capitalLetter : remainingLetters)
+
+parseTypeName :: ReadP Token
+parseTypeName = do
+    name <- parseTypeNameString
+    return (TypeName name)
+
+parseFieldName :: ReadP Token
+parseFieldName = do
+    name <- many1 (satisfy (\char -> char >= 'a' && char <= 'z'))
+    return (FieldName name)
+
+parseParLeft :: ReadP Token
+parseParLeft = do
+    satisfy (\char -> char == '(')
+    return ParLeft
+
+parseParRight :: ReadP Token
+parseParRight = do
+    satisfy (\char -> char == ')')
+    return ParRight
+
+parsePipe :: ReadP Token
+parsePipe = do
+    satisfy (\char -> char == '|')
+    return Pipe
+
+parseBraceLeft :: ReadP Token
+parseBraceLeft = do
+    satisfy (\char -> char == '{')
+    return BraceLeft
+
+parseBraceRight :: ReadP Token
+parseBraceRight = do
+    satisfy (\char -> char == '}')
+    return BraceRight
+
+parseColon :: ReadP Token
+parseColon = do
+    satisfy (\char -> char == ':')
+    return Colon
+    
+parseToken :: ReadP Token
+parseToken = do
+    skipSpaces
+    token <- parseTypeName <|> parseParLeft <|> parseParRight <|> parsePipe <|>
+                parseColon <|> parseBraceLeft <|> parseBraceRight
+    return token
+
+parseRHS :: ReadP [Token]
+parseRHS = do
+    tokens <- many1 parseToken
+    return tokens
+
+parseLHS :: ReadP [Char]
+parseLHS = do
+    skipSpaces
+    string "type"
+    skipSpaces
+    name <- parseTypeNameString
+    return name
+
+parseTypeDef :: ReadP ([Char], [Token])
+parseTypeDef = do
+    name <- parseLHS
+    skipSpaces
+    satisfy (\char -> char == '=')
+    rhs <- parseRHS
+    eof
+    return (name, rhs)
+
+tryToElmType :: ([Char], [Token]) -> Maybe ElmType
+tryToElmType (name, ast) =
+    let
+        variants = Data.List.Split.splitOn [Pipe] ast
+    in
+        Just (
+            ElmType
+            name
+            []
+            []
+            [] )
+
+data ElmBuiltIn = ElmString | ElmInt | ElmFloat
+    deriving Show
+
+data ElmType = ElmType
+    { name :: [Char]
+    , associated :: [ElmType]
+    , variants :: [ElmType]
+    , fields :: [([Char],ElmType)]
+    } | ElmBuiltIn
+    deriving Show
