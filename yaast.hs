475ce6171889a52c98469046d2865a2415734211
Nicolas Mohr <Nico.Mohr@gmx.net>
2019-12-06 21:06:21 +0100

    sum type parsing +type names now PascalCase

--- yaast.hs
+++ yaast.hs
@@ -1,20 +1,53 @@
 import Text.ParserCombinators.ReadP
 
-data MyType = MyType String [MyType] deriving Show
 
-mytype :: ReadP MyType
+-- parsers for top level sum type alternatives
+type SumType = [ElmType]
+
+-- parses a sumtype
+-- a | b | c
+sumtype :: ReadP SumType
+sumtype = do
+    skipSpaces
+    types <- sepBy1 mytree pipe
+    return types
+
+-- parses if next non-whitespace character is a pipe
+pipe = do
+    skipSpaces
+    char '|'
+
+-- parsers for the following type parameter associativity
+-- A
+-- ElmType 'A' []
+
+-- A B C
+-- ElmType 'A' [ElmType 'B' [], ElmType 'C']
+
+-- A (B C)
+-- ElmType A [(ElmType B [ElmType C []])]
+data ElmType = ElmType String [ElmType] deriving Show
+
+assoctree = myassociated +++ brackets mytype
+
+mytree = mytype +++ brackets mytype
+
+-- parses a type that can have type parameters
+mytype :: ReadP ElmType
 mytype = do
     skipSpaces
-    name <- char 'o'
+    name <- parseTypeName
     assoc <- many assoctree
-    return (MyType [name] assoc)
+    return (ElmType name assoc)
 
-myassociated :: ReadP MyType
+-- parses a type that can not have type parameters
+myassociated :: ReadP ElmType
 myassociated = do
     skipSpaces
-    name <- char 'o'
-    return (MyType [name] [])
+    name <- parseTypeName
+    return (ElmType name [])
 
+-- parses brackets
 brackets p = do
     skipSpaces
     char '('
@@ -23,20 +56,10 @@ brackets p = do
     char ')'
     return r
 
-assoctree = myassociated +++ brackets mytype
-mytree = mytype +++ brackets mytype
-
--- 
--- a b c
--- MyType a (cons b cons c cons nil)
--- collapse:
--- MyType a [b, c]
-
--- a (b c)
--- MyType a (cons ( b cons c cons nil ) cons nil)
--- collapse:
--- MyType a [(MyType b [c])]
-
--- a
--- MyType a cons nil
--- MyType a []
+-- parses a type name with correct capitalisation: PascalCase
+parseTypeName :: ReadP [Char]
+parseTypeName = do
+    skipSpaces
+    capitalLetter <- satisfy (\char -> char >= 'A' && char <= 'Z')
+    remainingLetters <- many (satisfy (\char -> char >= 'A' && char <= 'z'))
+    return (capitalLetter : remainingLetters)
