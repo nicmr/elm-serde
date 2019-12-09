4356114ee7af94a5e4189a87ef4a081a372ac076
Nicolas Mohr <Nico.Mohr@gmx.net>
2019-12-09 09:37:19 +0100

    added more examplary type definitions

--- examples/Example.elm
+++ examples/Example.elm
@@ -1,3 +1,21 @@
 module Example
 
-type Candy = KitKat | JeallyBean | ICS
+-- basic sum type
+type Roast = Dark | Medium | Light
+
+-- sum type with record
+type Candy = KitKat | ICS | Special { name : String }
+
+-- associated types of varying associativity
+type Drinks = Tea (Maybe String) | Coffee Roast
+
+-- type alias
+type alias Calories = List Int
+
+-- record alias
+type alias Model =
+    { count : Int
+    , favourite_candy : Candy
+    , friends : List String
+    , caloryHistory : Calories
+    }
